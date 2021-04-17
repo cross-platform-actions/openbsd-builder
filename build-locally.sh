69aeaad27f0351e7e90651169a8034c2c02b2bda
Jacob Carlborg <doob@me.com>
2021-04-17 20:59:32 +0200

    Move arch specific to own files

--- build-locally.sh
+++ build-locally.sh
@@ -12,7 +12,8 @@ rm -rf packer_cache
 
 PACKER_LOG=1 packer build \
   -var-file "var_files/common.pkrvars.hcl" \
-  -var-file "var_files/$OS_VERSION/common.pkrvars.hcl" \
   -var-file "var_files/$ARCHITECTURE.pkrvars.hcl" \
+  -var-file "var_files/$OS_VERSION/common.pkrvars.hcl" \
+  -var-file "var_files/$OS_VERSION/$ARCHITECTUREpkrvars.hcl" \
   -var-file var_files/local-macos.pkrvars.hcl \
   openbsd.pkr.hcl
---
+++ var_files/6.8/x86-64.pkrvars.hcl
@@ -0,0 +1,2 @@
+architecture = "x86-64"
+checksum = "sha256:d3603291fa50bcf056fa304acf9311556331598f31dfbbbc6797ae88f43c948b"
