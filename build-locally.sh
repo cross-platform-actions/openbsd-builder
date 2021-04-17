4dbb52bdd3283d39f049e5c6cd5bfad5797d51d2
Jacob Carlborg <doob@me.com>
2021-04-17 20:41:09 +0200

    Generalize github and local-macos vars files

--- build-locally.sh
+++ build-locally.sh
@@ -8,10 +8,10 @@ ARCHITECTURE="$2"
 # mkdir -p "$PROJECT_ROOT_PATH/packer_cache_backup"
 # cp "$PROJECT_ROOT_PATH/packer_cache"/*.img "$PROJECT_ROOT_PATH/packer_cache_backup"
 
-rm -rf "$PROJECT_ROOT_PATH/packer_cache"
+rm -rf packer_cache
 
 PACKER_LOG=1 packer build \
   -var-file "$OS_VERSION/common.pkrvars.hcl" \
-  -var-file "$OS_VERSION/local-macos.pkrvars.hcl" \
   -var-file "$ARCHITECTURE.pkrvars.hcl" \
-  "$PROJECT_ROOT_PATH/openbsd.pkr.hcl"
+  -var-file local-macos.pkrvars.hcl \
+  openbsd.pkr.hcl
