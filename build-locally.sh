1196cc6f1912f8324da00dcc1d831987a943eb6b
Jacob Carlborg <doob@me.com>
2021-04-19 21:11:47 +0200

    Move arch specific files to their own directories

--- build-locally.sh
+++ build-locally.sh
@@ -12,9 +12,9 @@ rm -rf packer_cache
 
 PACKER_LOG=1 packer build \
   -var-file "var_files/common.pkrvars.hcl" \
-  -var-file "var_files/$ARCHITECTURE.pkrvars.hcl" \
+  -var-file "var_files/local-macos.pkrvars.hcl" \
+  -var-file "var_files/$ARCHITECTURE/common.pkrvars.hcl" \
+  -var-file "var_files/$ARCHITECTURE/local-macos.pkrvars.hcl" \
   -var-file "var_files/$OS_VERSION/common.pkrvars.hcl" \
   -var-file "var_files/$OS_VERSION/$ARCHITECTURE.pkrvars.hcl" \
-  -var-file "var_files/$ARCHITECTURE-local-macos.pkrvars.hcl" \
-  -var-file var_files/local-macos.pkrvars.hcl \
   openbsd.pkr.hcl
