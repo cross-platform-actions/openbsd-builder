2846a9585c1cb19ec706fee081db2837eb98c882
Evan Sherwood <evan@sherwood.io>
2024-08-24 10:35:05 -0700

    Support snapshots

--- build.sh
+++ build.sh
@@ -5,10 +5,15 @@ set -euxo pipefail
 OS_VERSION="$1"; shift
 ARCHITECTURE="$1"; shift
 
-packer build \
-  -var os_version="$OS_VERSION" \
-  -var-file "var_files/common.pkrvars.hcl" \
-  -var-file "var_files/$ARCHITECTURE.pkrvars.hcl" \
-  -var-file "var_files/$OS_VERSION/$ARCHITECTURE.pkrvars.hcl" \
-  "$@" \
-  openbsd.pkr.hcl
+flags=(
+  "-var"      "os_version=$OS_VERSION"
+  "-var-file" "var_files/common.pkrvars.hcl"
+  "-var-file" "var_files/$ARCHITECTURE.pkrvars.hcl"
+)
+
+if [ -e "var_files/$OS_VERSION/$ARCHITECTURE.pkrvars.hcl" ]; then
+  flags+=("-var-file")
+  flags+=("var_files/$OS_VERSION/$ARCHITECTURE.pkrvars.hcl")
+fi
+
+packer build "${flags[@]}" "$@" openbsd.pkr.hcl
